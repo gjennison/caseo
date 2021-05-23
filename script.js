4f31eb4e0e1c77496621211f7d80c2ffcdbc78f2
gjennison <georjennison@gmail.com>
2021-05-24 09:01:27 +1200

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let nav = document.querySelector('nav')
+let menu = document.querySelector('.links')
+let hamburger = document.querySelector('.hamburger')
+let cross = document.querySelector('.close')
+
+
+hamburger.addEventListener('click', openMenu)
+cross.addEventListener('click', closeMenu)
+
+function openMenu(){
+    menu.style.animation = "openMenuAnimation 0.5s"
+
+    menu.classList.remove('closedMenu')
+    menu.classList.add('openMenu')
+
+    hamburger.style.display = "none"
+    cross.style.display = "block"
+}
+
+function closeMenu(){
+    menu.style.animation = "closeMenuAnimation 0.5s"
+    
+    menu.classList.remove('openMenu')
+    menu.classList.add('closedMenu')
+
+    hamburger.style.display = "block"
+    cross.style.display = "none"
+}
+
+document.addEventListener('scroll', function(e) {
+    if(window.scrollY >= 50){
+        nav.classList.add('secondaryScheme')
+        nav.classList.remove('primaryScheme')
+    }
+    if(window.scrollY < 50){
+        nav.classList.add('primaryScheme')
+        nav.classList.remove('secondaryScheme')
+    }
+})
